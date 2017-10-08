7a2850fc9512b21550ebe9d45242122a00c2df54
coder-god <407410014@qq.com>
2017-10-08 17:10:51 +0800

    add comments for class Utils and udate methods

--- src/my/bird/flappy/Utils.java
+++ src/my/bird/flappy/Utils.java
@@ -1,20 +1,34 @@
 package my.bird.flappy;
 
+/**
+ * a util class 
+ * 
+ * @author HJS
+ *
+ * @date 2017年10月8日
+ */
 public class Utils {
     
-    private static Wall[] walls;
+    private static Wall[] walls;    // in one frame, there are several walls 
     
-    private static int pivot = 0;
+    private static int pivot = 0;   // a pivot for walls
     
-    private static double wallSpeed = 0.01;
+    private static double wallSpeed = 0.01; // wall's retreating speed 
+    
     private Utils() { }   // this class should not be initialized
     
+    /**
+     * clean game's state to original state 
+     */
     public static void clear() {
         pivot = 0;
         walls = null;
         wallSpeed = 0.01;
     }
     
+    /**
+     * as walls should be changing, this method generate new walls still   
+     */
     public static Wall[] getWalls() {
         if(walls == null) {
             walls = new Wall[3];
@@ -23,22 +37,15 @@ public class Utils {
             }
         }
         for(Wall w: walls) {
-            if(w.getxAxis() >= 0) {
+            if(w.getxAxis() >= 0) { // if wall is in the frame, then move backward 
                 w.setxAxis(w.getxAxis() - wallSpeed);
             }
-            else {
+            else {  // if wall is not in the frame, generate a new wall to replace old wall
                 walls[pivot] = new Wall(1.0);
                 if(pivot < walls.length - 1)    pivot++;
                 else                            pivot = 0;
             }
         }
-        
-        
         return walls;
     }
-    
-    public static int getPivot() {
-        return pivot;
-    }
-    
 }
